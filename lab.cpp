ca2b428e6cf4bdecc00a9e1d16bbcbf380792765
Margarita86946 <melikyanm22@gmail.com>
2024-10-22 00:39:26 +0400

    Add files via upload
    adding lab 5

--- lab.cpp
+++ lab.cpp
@@ -1,85 +1,34 @@
 #include <iostream>
-#include <cmath>
-#include <vector>
 #include <string>
 
 using namespace std;
 
-void fillMatrixInRows(vector<vector<char>>& matrix, const string& str, int size) {
-    int idx = 0;
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            if (idx < str.length()) {
-                matrix[i][j] = str[idx++];
-            } else {
-                matrix[i][j] = ' ';
-            }
+    string runLengthEncode(const string &input) {
+    string encoded = "";
+    int n = input.length();
+
+    for (int i = 0; i < n; i++) {
+        int count = 1;
+        while (i < n - 1 && input[i] == input[i + 1]) {
+            count++;
+            i++;
         }
+
+        encoded += input[i];
+        encoded += to_string(count);
     }
-}
 
-string readMatrixByColumns(const vector<vector<char>>& matrix, int size) {
-    string encoded_string = "";
-    for (int j = 0; j < size; ++j) {
-        for (int i = 0; i < size; ++i) {
-            encoded_string += matrix[i][j];
-        }
-    }
-    return encoded_string;
-}
-
-string encodeString(const string& str, int matrix_size) {
-    int size = ceil(sqrt(matrix_size)); // Calculate matrix size
-    vector<vector<char>> matrix(size, vector<char>(size, ' '));
-    fillMatrixInRows(matrix, str, size);
-    return readMatrixByColumns(matrix, size);
-}
-
-void fillMatrixByColumns(vector<vector<char>>& matrix, const string& str, int size) {
-    int idx = 0;
-    for (int j = 0; j < size; ++j) {
-        for (int i = 0; i < size; ++i) {
-            if (idx < str.length()) {
-                matrix[i][j] = str[idx++];
-            } else {
-                matrix[i][j] = ' ';
-            }
-        }
-    }
-}
-
-string readMatrixByRows(const vector<vector<char>>& matrix, int size) {
-    string decoded_string = "";
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            decoded_string += matrix[i][j];
-        }
-    }
-    return decoded_string;
-}
-
-string decodeString(const string& str, int matrix_size) {
-    int size = ceil(sqrt(matrix_size));
-    vector<vector<char>> matrix(size, vector<char>(size, ' '));
-    fillMatrixByColumns(matrix, str, size);
-    return readMatrixByRows(matrix, size);
+    return encoded;
 }
 
 int main() {
-
-    string user_string;
-    cout << "Enter the string to encode: ";
-    getline(cin, user_string);
+    string input;
     
-    int matrix_size;
-    cout << "Enter the matrix size (n*n): ";
-    cin >> matrix_size;
+    cout << "Enter a string: ";
+    cin >> input;
 
-    string encoded = encodeString(user_string, matrix_size);
-    cout << "Encoded string: " << encoded << endl;
-
-    string decoded = decodeString(encoded, matrix_size);
-    cout << "Decoded string: " << decoded << endl;
+    string encodedString = runLengthEncode(input);
+    cout << "Encoded string: " << encodedString << endl;
 
     return 0;
 }
